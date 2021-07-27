98f6e74bc308146826e02c7168eaf5cacc533eaf
Pauwel De Wilde <pauweldewilde@gmail.com>
2021-07-27 16:10:16 +0200

    Update schemas.py

--- model/schemas.py
+++ model/schemas.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import reqparse
 
-class get_parameters():
-    def __init___(self):
-        self.__area = parser.parse_args().get("area", "None")
-        self.__postalCode = parser.parse_args().get("postalCode", "None")
 
-    def get(self):
+parser = reqparse.RequestParser()
+parser.add_argument("area", float, required = True, help = "Not all required data are given")
+parser.add_argument("postalCode", int, required = True, help = "Not all required data are given")
+response = {}
+
+class get_parameters():
+    def area(self):
+        self.__area = parser.parse_args().get("area", "None")
+        if self.__area:
+            response["area"] = jsonify(self.__area)
         
+    def postal_code(self):
+        self.__postalCode = parser.parse_args().get("postalCode", "None")
+        if len(self.__postalCode) == 4:
+            response["postalCode"] = jsonify(self.__postalCode)
+        else:
+            return jsonify({"Warning": "Postal code exists of 4 characters"})
+
+    
