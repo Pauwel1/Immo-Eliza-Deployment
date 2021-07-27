b77838c8922d1af6059cb9de2105c55bacaaa481
Pauwel De Wilde <pauweldewilde@gmail.com>
2021-07-27 16:54:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
+import os
 from flask import Flask, request, jsonify
+from model.schemas import get_parameters
+
+
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
-def respond():
+
+@app.route("/", methods = ["GET"])
+def check():
     return "Alive!"
+
+@app.route("/predict", methods = ["POST", "GET"])
+def respond():
+    df = get_parameters.response()
+    print(f"Got {df}")
+    return jsonify(df)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host = "0.0.0.0", threaded = True, port = port)
