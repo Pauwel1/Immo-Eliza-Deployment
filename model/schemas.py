e7429cbcd79f50e2ed09a9443bcac0577d665860
Pauwel De Wilde <pauweldewilde@gmail.com>
2021-07-27 16:56:46 +0200

    Without flask_restful

--- model/schemas.py
+++ model/schemas.py
@@ -1,20 +1,16 @@
 from flask import Flask, request, jsonify
-from flask_restful import reqparse
 
 
-parser = reqparse.RequestParser()
-parser.add_argument("area", float, required = True, help = "Not all required data are given")
-parser.add_argument("postalCode", int, required = True, help = "Not all required data are given")
 response = {}
 
 class get_parameters():
     def area(self):
-        self.__area = parser.parse_args().get("area", "None")
+        self.__area = request.args().get("area", "None")
         if self.__area:
             response["area"] = self.__area
         
     def postal_code(self):
-        self.__postalCode = parser.parse_args().get("postalCode", "None")
+        self.__postalCode = request.args().get("postalCode", "None")
         if len(self.__postalCode) == 4:
             response["postalCode"] = self.__postalCode
         else:
