f06be1a7fac93fc52fd56bca93903acce69563a3
Pauwel De Wilde <pauweldewilde@gmail.com>
2021-07-29 10:19:48 +0200

    Error handling update

--- preprocessing/schemas.py
+++ preprocessing/schemas.py
@@ -11,8 +11,8 @@ app = Flask(__name__)
 class get_parameters():
     def area():
         area = request.args.get("area", None)
-        if area == None:
-            abort(400, description = "Area not given")
+        if (area == None):
+            abort(400, description = "Area not given (only numbers separated by a '.' are allowed)")
         else:
             return area
         
@@ -21,20 +21,31 @@ class get_parameters():
         if len(postalCode) == 4:
             return postalCode
         else:
-            abort(400, description = "Postal code should exist of 4 intigers")
+            abort(400, description = "Postal code should exist of 4 intigers, without space")
     
     def subtype_property():
         subtypeProperty = request.args.get("subtypeProperty", None)
-        # There should be a limited list to select from, that will be transformed to dummies with values 1 - ?
-        if subtypeProperty:
+        # There should be a limited list to select from, that will be transformed to dummies with values 1 - 22
+        lst = [
+            "APARTMENT", "HOUSE", "MANSION", 
+            "VILLA", "STUDIO", "HOUSE_GROUP", 
+            "EXCEPTIONAL_PROPERTY", "MIXED_USE_BUILDING", "APARTMENT_BLOCK",
+            "BUNGALOW", "CASTLE", "COUNTRY_HOUSE", 
+            "TOWN_HOUSE", "MANOR_HOUSE", "GROUND_FLOOR",
+            "PENTHOUSE", "KOT", "CHALET",
+            "FARMHOUSE", "PAVILION", "DUPLEX",
+            "LOFT", "SERVICE_FLAT", "TRIPLEX"
+            ]
+        if subtypeProperty in lst:
             return subtypeProperty
         else:
             abort(400, description = "Choose one of the property type options")
 
     def building_condition():
         buildingCondition = request.args.get("buildingCondition", None)
-        # There should be a limited list to select from, that will be transformed to dummies with values 1 - 5
-        if buildingCondition:
+        # There should be a limited list to select from, that will be transformed to dummies with values 1 - 6
+        lst2 = ["GOOD", "TO_RESTORE", "TO_BE_DONE_UP", "AS_NEW", "JUST_RENOVATED", "TO_RENOVATE"]
+        if buildingCondition in lst2:
             return buildingCondition
         else:
             abort(400, description = "Choose one of the building condition options")
